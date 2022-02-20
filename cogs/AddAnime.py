61cb2867e54dde7f07928a3c404f6b2b1ae93a1b
lgf2111 <lgf2111@gmail.com>
2022-02-20 23:13:20 +0800

    AddAnime check if anime's ald in list

--- cogs/AddAnime.py
+++ cogs/AddAnime.py
@@ -19,38 +19,43 @@ class AddAnime(commands.Cog):
         if len(cmd.split()) == 1:
             await ctx.send('Enter the anime link of any episode beside this command.\n'
                             'Example: gogo.add https://gogoanime.film/shingeki-no-kyojin-the-final-season-part-2-episode-1')
+
         elif len(cmd.split()) > 2:
             await ctx.send('Invalid anime link.\n'
                             'Enter the anime link of any episode beside this command.\n'
                             'Example: gogo.add https://gogoanime.film/shingeki-no-kyojin-the-final-season-part-2-episode-1')
+                            
         else:
             link = cmd.split()[1]
             anime = link[link.find('https://gogoanime.film/')+23:link.rfind('-episode')]
             title = ' '.join([_.capitalize() for _ in anime.split('-')])
-            base_url = f'https://gogoanime.film/{anime}'
-            counter = 0
-            while True:
-                counter += 1
-                url = f'{base_url}-episode-{counter}'
-                session = HTMLSession()
-                data = session.get(url)
-                if data.html.find('.entry-title'):
-                    if counter == 1:
-                        await ctx.send(f'{title} does not exist.')
-                    else:
-                        with open('db.txt', 'r') as f:
-                            db = eval(f.read())
-                        gld_db = db[gld.id][1]
-                        if usr.id not in gld_db:
-                            gld_db[usr.id] = []
-                        gld_db[usr.id].append([title,counter])
-                        db[gld.id][1] = gld_db
-                        with open('db.txt', 'w') as f:
-                            f.write(str(db))
-                        logging.info(f'{usr} has added tracker for {title}.')
-                        await ctx.send(f'{title} is currently on episode {counter-1}.\n'
-                                        f'New episodes will be notified starting from episode {counter}.')
-                    break
+            with open('db.txt', 'r') as f:
+                db = eval(f.read())
+            gld_db = db[gld.id][1]
+            if title in [_[0] for _ in gld_db[usr.id]]:
+                await ctx.send(f'{title} is already in your list.')
+            else:
+                base_url = f'https://gogoanime.film/{anime}'
+                counter = 0
+                while True:
+                    counter += 1
+                    url = f'{base_url}-episode-{counter}'
+                    session = HTMLSession()
+                    data = session.get(url)
+                    if data.html.find('.entry-title'):
+                        if counter == 1:
+                            await ctx.send(f'{title} does not exist.')
+                        else:
+                            if usr.id not in gld_db:
+                                gld_db[usr.id] = []
+                            gld_db[usr.id].append([title,counter])
+                            db[gld.id][1] = gld_db
+                            with open('db.txt', 'w') as f:
+                                f.write(str(db))
+                            logging.info(f'{usr} has added tracker for {title}.')
+                            await ctx.send(f'{title} is currently on episode {counter-1}.\n'
+                                            f'New episodes will be notified starting from episode {counter}.')
+                        break
 
 
 def setup(client):
