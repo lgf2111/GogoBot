c8df7f1d4b3c2f61546407327fdf0422dfa99e97
lgf2111 <lgf2111@gmail.com>
2022-02-20 22:52:27 +0800

    updated setup ui

--- cogs/Setup.py
+++ cogs/Setup.py
@@ -14,16 +14,32 @@ class Setup(commands.Cog):
         msg = ctx.message
         gld = ctx.guild
         cmd = msg.content
-        # gld = int(cmd[cmd.find('/channels/')+10:cmd.rfind('/')])
-        chn = int(cmd[cmd.rfind('/')+1:])
-        # if gld.id != cmd gld
-        with open('db.txt', 'r') as f:
-            db = eval(f.read())
-        db[gld.id] = [chn, {}]
-        with open('db.txt', 'w') as f:
-            f.write(str(db))
-        chn = discord.utils.get(ctx.guild.channels, id=chn)
-        await ctx.send(f'Notifications will be sent to {chn.name}.')
+        
+        if len(cmd.split()) == 1:
+            await ctx.send('Enter the channel link beside this command.\n'
+                           'Example: gogo.setup https://discord.com/channels/123456789012345678/123456789012345678')
+
+        elif len(cmd.split()) > 2:
+            await ctx.send('Invalid channel link.\n'
+                           'Enter the channel link beside this command.\n'
+                           'Example: gogo.setup https://discord.com/channels/123456789012345678/123456789012345678')
+                           
+        else:
+            gld_id = int(cmd[cmd.find('/channels/')+10:cmd.rfind('/')])
+            chn = int(cmd[cmd.rfind('/')+1:])
+            if gld.id == gld_id:
+                with open('db.txt', 'r') as f:
+                    db = eval(f.read())
+                if db.get(gld.id):
+                    db[gld.id][0] = chn
+                else:
+                    db[gld.id] = [chn, {}]
+                with open('db.txt', 'w') as f:
+                    f.write(str(db))
+                chn = discord.utils.get(ctx.guild.channels, id=chn)
+                await ctx.send(f'Notifications will be sent at {chn.name}.')
+            else:
+                await ctx.send('Please copy channel link from this server.')
 
 def setup(client):
     client.add_cog(Setup(client))
